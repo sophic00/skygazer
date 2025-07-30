b5920bcea5dc5d71e73ed06fef37adba083ed97c
Vaibhav Sijaria <139199971+sophic00@users.noreply.github.com>
2025-07-31 01:23:14 +0530

    feat: added home page content

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import IconHuggingFace from "@/assets/icons/IconHuggingFace.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
@@ -23,17 +24,23 @@ export const SOCIALS: Social[] = [
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/vaibhav-sijaria",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    icon: IconLinkedin,
+  },
+  {
+    name: "HuggingFace",
+    href: "https://huggingface.co/sophic00",
+    linkTitle: `${SITE.title} on HuggingFace`,
+    icon: IconHuggingFace,
+  },
   // {
   //   name: "X",
   //   href: "https://x.com/username",
   //   linkTitle: `${SITE.title} on X`,
   //   icon: IconBrandX,
-  // },
-  // {
-  //   name: "LinkedIn",
-  //   href: "https://www.linkedin.com/in/username/",
-  //   linkTitle: `${SITE.title} on LinkedIn`,
-  //   icon: IconLinkedin,
   // },
   // {
   //   name: "Mail",
